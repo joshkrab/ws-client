99d4c8de8e3dadfe6638ad2159881cd97f6f222e
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-11-08 21:34:27 +0200

    rr

--- src/components/Websocket.tsx
+++ src/components/Websocket.tsx
@@ -1,10 +1,17 @@
-import React, {useContext, useEffect, useState} from 'react'
-import {WebsocketContext} from '../contexts/WebsocketContext'
+import React, {useContext, useEffect, useState} from 'react';
+import { v4 as uuid } from 'uuid';
+import {WebsocketContext} from '../contexts/WebsocketContext';
 import Button from './common/Button/Button';
 import Input from './common/Input/Input';
 
+type MessagePayload = {
+	message: string;
+	content: string;
+}
+
 export const Websocket = () => {
 	const [value, setValue] = useState('');
+	const [messages, setMessages] = useState<MessagePayload[]>([]);
 	const socket = useContext(WebsocketContext);
 
 	useEffect(() => {
@@ -12,9 +19,10 @@ export const Websocket = () => {
 			console.log('Connected!');
 		})
 
-		socket.on('onMessage', (data) => {
+		socket.on('onMessage', (data: MessagePayload) => {
 			console.log('onMessage event received:');
 			console.log(data);
+			setMessages((prev) => [...prev, data]);
 		})
 
 		// When unmount component: close and open, otherwise it is mounted twice:
@@ -35,14 +43,22 @@ export const Websocket = () => {
 		<div>
 			<div>
 				<h1>Websocket component</h1>
+				<div>{
+					messages.length === 0 ? <div>No messages</div> : <div>{
+						messages.map(msg => { 
+							const unique_id = uuid();
+							return <div key={unique_id}>{ msg.content}</div>
+						})
+					}</div>
+				}</div>
 				<Input
-					labeltext={'label text'}
-					placeholder={'placeholder'}
+					//labeltext={'label text'}
+					placeholder={'Input your message'}
 					value={value}
 					onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
 				/>
 				<Button
-					buttontext={'Submit'}
+					buttontext={'Send'}
 					onClick={onSubmit}
 				/>
 			</div>
